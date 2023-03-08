037c785e4b78f4eaf9b1876a069c5b87657875a3
Bel <114267626+BA-BEL@users.noreply.github.com>
2023-03-08 22:05:22 +1100

    Finished bubble chart with dynamic capabilities

--- static/js/app.js
+++ static/js/app.js
@@ -9,68 +9,96 @@ d3.json(url).then((data) => console.log(data));
 d3.json(url).then(init);
 
                   
-                  //////// Functions ////////
+                        //////// Functions ////////
 
-// Initializer funciton
+///////// Initializer function
 function init(data){
-let sample = data.samples.map((arg) => arg.sample_values);
-let otuids = data.samples.map((arg) => arg.otu_ids);
-let otulabels = data.samples.map(arg => arg.otu_labels);
 
-let toptensamples = [];
-let toptenotuids = [];
-let toptenotulabels = [];
+    // Get data
+    let sample = data.samples.map((arg) => arg.sample_values);
+    let otuids = data.samples.map((arg) => arg.otu_ids);
+    let otulabels = data.samples.map(arg => arg.otu_labels);
 
-//// Plot initial dataset
+    let toptensamples = [];
+    let toptenotuids = [];
+    let toptenotulabels = [];
 
-for(let i = 0; i < 10;i++){
-    toptensamples.push(sample[0][i]);
-    toptenotuids.push(String(otuids[0][i]));
-    toptenotulabels.push(otulabels[0][i]);
+// –––––––––––––––––––
+    // Plot initial dataset's bar chart
+
+    for(let i = 0; i < 10;i++){
+        toptensamples.push(sample[0][i]);
+        toptenotuids.push(String(otuids[0][i]));
+        toptenotulabels.push(otulabels[0][i]);
+    }
+
+    // Reverse order of arrays
+    toptensamples.reverse();
+    toptenotuids.reverse();
+    toptenotulabels.reverse();
+
+    // Plot
+    let plotdata = [{
+        x:toptensamples,
+        y:toptenotuids.map((arg) => `OTU ${arg}`),
+        text:toptenotulabels,
+        type:"bar",
+        orientation:"h"
+    }];
+
+    Plotly.newPlot("bar",plotdata);
+
+// –––––––––––––––––––
+    // Plot initial dataset's bubble plot
+    let bubbledata = [{
+        x:otuids[0],
+        y:sample[0],
+        mode:"markers",
+        text:otulabels[0],
+        marker:{
+            color:otuids[0],
+            size:sample[0]
+        },
+        text:otulabels[0],
+    }];
+
+    let bubblelayout = {
+        xaxis:{title:{text: "OTU ID"}}
+    };
+
+    Plotly.newPlot("bubble",bubbledata, bubblelayout)
+// –––––––––––––––––––
+
+    // Fill dropdown menu with options
+    let option;
+
+    for(i = 0; i < data.samples.length; i++){
+        option = d3.select("#selDataset").append("option");
+        option_value = option.attr("value",Object.keys(data.samples)[i])
+        option_text = option.text(data.samples[i].id)
+    }
+
+// –––––––––––––––––––
+    // Fill default demographic info
+
+    for(let i = 0; i < Object.keys(data.metadata[0]).length; i++){
+        console.log(Object.keys(data.metadata[0]))
+
+        d3.select("#sample-metadata").append("p").html(
+            `${Object.keys(data.metadata[0])[i]}: ${Object.values(data.metadata[0])[i]}`)
+    }
+// –––––––––––––––––––
 }
 
-    // console.log(`Top ten samples: ${toptensamples}`);
-    // console.log(`Top ten otu ids: ${toptenotuids}`);
-    // console.log(`Top ten otu labels: ${toptenotulabels}`);
-
-// Reverse order of arrays
-toptensamples.reverse();
-toptenotuids.reverse();
-toptenotulabels.reverse();
-
-let plotdata = [{
-    x:toptensamples,
-    y:toptenotuids.map((arg) => `OTU ${arg}`),
-    text:toptenotulabels,
-    type:"bar",
-    orientation:"h"
-}];
-
-Plotly.newPlot("bar",plotdata);
-
-//// Fill dropdown menu with options
-let option;
-
-for(i = 0; i < data.samples.length; i++){
-    option = d3.select("#selDataset").append("option");
-    option_value = option.attr("value",Object.keys(data.samples)[i])
-    option_text = option.text(data.samples[i].id)
-}
-
-}
-
-// Update plot function
-
-
-
+////////// Update plot function
 
 function optionChanged(this_value){
-
-    // d3.json(url).then((data)=>console.log(data))
 
     let value = this_value;
 
     d3.json(url).then(function(data){
+    
+        //// Update dataset
 
         let sample = data.samples.map((arg) => arg.sample_values);
         let otuids = data.samples.map((arg) => arg.otu_ids);
@@ -80,8 +108,9 @@ function optionChanged(this_value){
         let toptenotuids = [];
         let toptenotulabels = [];
     
-        // Update dataset
-    
+        
+// ––––––––––––––––––– 
+    //  Update Bar Chart   
         // Get top 10 values
         for(let i = 0; (i < 10 && i < sample[value].length);i++){
             toptensamples.push(sample[value][i]);
@@ -94,10 +123,6 @@ function optionChanged(this_value){
         toptenotuids.reverse();
         toptenotulabels.reverse();
 
-        // console.log(`Top ten samples: ${toptensamples}`);
-        // console.log(`Top ten otu ids: ${toptenotuids}`);
-        // console.log(`Top ten otu labels: ${toptenotulabels}`);
-
         let plotdata = {
                 x:toptensamples,
                 y:toptenotuids.map((arg) => `OTU ${arg}`),
@@ -106,9 +131,36 @@ function optionChanged(this_value){
                 orientation:"h"
             };
 
+
         Plotly.deleteTraces("bar",[0]);
         Plotly.addTraces("bar", plotdata);
     
+
+// –––––––––––––––––––
+    // Update Bubble plot
+    let bubbledata = [{
+        x:otuids[value],
+        y:sample[value],
+        mode:"markers",
+        text:otulabels[value],
+        marker:{
+            color:otuids[value],
+            size:sample[value]
+        },
+        text:otulabels[value],
+    }];
+
+ 
+    Plotly.deleteTraces("bubble",[0]);
+    Plotly.addTraces("bubble", bubbledata);
+// –––––––––––––––––––
+
+        //// Update demographic info
+        d3.select("#sample-metadata").html("")
+        for(let i = 0; i < Object.keys(data.metadata[value]).length; i++){    
+            d3.select("#sample-metadata").append("p").html(
+                `${Object.keys(data.metadata[value])[i]}: ${Object.values(data.metadata[value])[i]}`)
+        }
     });
 }
 
